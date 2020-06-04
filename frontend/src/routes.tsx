2059ae142c1f4acdde6cd87e6ad606363234b385
mateusamarall <mateus.amaral018@gmail.com>
2020-06-04 18:17:16 -0300

    route of sucess created

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home";
 import Point from "./Pages/CreatePoint";
+import Sucess from "./Pages/SucessPage";
 const Routes = () => {
   return (
     <BrowserRouter>
       <Route exact component={Home} path="/" />
       <Route component={Point} path="/create-point" />
+      <Route component={Sucess} path="/sucess" />
     </BrowserRouter>
   );
 };
