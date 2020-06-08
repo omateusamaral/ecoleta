5dfec7e83ccbc2d499d150b305a4b2e1914fa201
mateusamarall <mateus.amaral018@gmail.com>
2020-06-08 14:39:17 -0300

    Configuration for upload

--- frontend/src/Pages/CreatePoint/index.tsx
+++ frontend/src/Pages/CreatePoint/index.tsx
@@ -6,6 +6,8 @@ import axios from "axios";
 import { LeafletMouseEvent } from "leaflet";
 import api from "../../services/api";
 
+import Dropzone from "../../Components/DropZone";
+
 import "./CreatePoint.css";
 import logo from "../../assets/logo.svg";
 interface Item {
@@ -40,7 +42,7 @@ const Point = () => {
     whatsapp: "",
   });
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
-
+  const [selectedFile, setSelectedFile] = useState<File>();
   const history = useHistory();
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -123,16 +125,21 @@ const Point = () => {
     const [latitude, longitude] = selectedPosistion;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("email", email);
+
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+
+    if (selectedFile) {
+      data.append("image", selectedFile);
+    }
 
     await api.post("points", data);
 
@@ -153,6 +160,7 @@ const Point = () => {
           Cadastro do <br />
           ponto de coleta
         </h1>
+        <Dropzone onFileUpload={setSelectedFile} />
         <fieldset>
           <legend>
             <h2>dados</h2>
